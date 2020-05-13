8314f12872bcacc467d196194ead4bb1cedc8179
lionet <jialecode@qq.com>
2020-05-13 20:40:37 +0800

    add

--- view/js/tool.js
+++ view/js/tool.js
@@ -320,6 +320,27 @@ function delCookie(name){
         document.cookie= name + "="+cval+";expires="+exp.toGMTString();
 }
 
+(function() {
+  var uaResult = (window && window.navigator && window.navigator.userAgent ? window.navigator.userAgent : "");
+  if (uaResult && !!uaResult.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+    if ("standalone" in window.navigator && window.navigator.standalone) {
+      var element;
+      /** @type {boolean} */
+      var l = false;
+      document.addEventListener("click", function(event) {
+        /** @type {(EventTarget|null)} */
+        element = event.target;
+        for (; element.nodeName !== "A" && element.nodeName !== "HTML";) {
+          element = element.parentNode;
+        }
+        if ("href" in element && element.href.indexOf("http") !== -1 && (element.href.indexOf(document.location.host) !== -1 || l)) {
+          event.preventDefault();
+          document.location.href = element.href;
+        }
+      }, false);
+    }
+  }
+})();
 
 module.exports = {
 	toObj,

74cefb5e0397cc294a9b197f9e6493312b0055a0
lionet <3339496031@qq.com>
2020-08-06 21:11:55 +0800

    fix

--- config/request.js
+++ config/request.js
@@ -7,5 +7,5 @@ module.exports = {
 	encoding: null,
 	headers: {},
 	proxyUri:
-		"http://api.hailiangip.com:8422/api/getIp?type=1&num=100&pid=&unbindTime=1800&cid=&orderId=O20061522100120451466&time=1596701865&sign=69f91d6e7141e1da8a7d2284fc4853d5&noDuplicate=1&dataType=0&lineSeparator=0&singleIp=0",
+		"",
 };
